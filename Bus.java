dd1d16e7ca61bb54a8fc33441debeac2efc8dbb5
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-20 17:09:00 +0300

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -1,9 +1,5 @@
 package com.example.omnibus;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 public class Bus {
