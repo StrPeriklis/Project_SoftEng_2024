26b14d494798489207834eae320796dc40951d00
StrPeriklis <129185321+StrPeriklis@users.noreply.github.com>
2024-05-20 16:44:54 +0300

    Add files via upload

---
+++ example/omnibus/OmnibusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omnibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
