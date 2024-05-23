fe1fde0af80c7139792b78bb72f123bf029534a8
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-23 19:22:01 +0300

    Update Ticket.java

--- code_all/Ticket.java
+++ code_all/Ticket.java
@@ -2,7 +2,12 @@ package com.example.omnibus;
 
 import java.sql.Date;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Ticket {
