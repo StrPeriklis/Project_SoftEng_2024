fa0b683e8da81c7414dbe3fde651f6c96ea4a155
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-23 21:34:40 +0300

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -17,5 +17,53 @@ public class Bus {
 	private Integer xoritikotita;
 	private String marka;
 	private String model;
-    private float lat;
-    private float lng;
+        private float lat;
+        private float lng;
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLng() {
+		return lng;
+	}
+	public void setLng(float lng) {
+		this.lng = lng;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getArithmos_kykloforias() {
+		return arithmos_kykloforias;
+	}
+	public void setArithmos_kykloforias(String arithmos_kykloforias) {
+		this.arithmos_kykloforias = arithmos_kykloforias;
+	}
+	public String getEtos_kykloforias() {
+		return etos_kykloforias;
+	}
+	public void setEtos_kykloforias(String etos_kykloforias) {
+		this.etos_kykloforias = etos_kykloforias;
+	}
+	public Integer getKyvika() {
+		return kyvika;
+	}
+	public void setKyvika(Integer kyvika) {
+		this.kyvika = kyvika;
+	}
+	public Integer getXoritikotita() {
+		return xoritikotita;
+	}
+	public void setXoritikotita(Integer xoritikotita) {
+		this.xoritikotita = xoritikotita;
+	}
+	public String getMarka() {
+		return marka;
+	}
+	public void setMarka(String marka) {
+		this.marka = marka;
+	}
