1c0c63d6cb62998fef3e7bc25e48d59018513cb9
PERIKLIS STRAVODIMOS <129185321+StrPeriklis@users.noreply.github.com>
2024-05-23 19:57:38 +0300

    Delete UserRepository.java

--- UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.omnibus;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository  extends CrudRepository<User,Integer>{
-	
-	 User findByEmailAndPassword(String email, String password);
-}
