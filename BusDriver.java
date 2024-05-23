c41745f3a3ead53753543a5fa5c8dc81da567f6f
StrPeriklis <129185321+StrPeriklis@users.noreply.github.com>
2024-05-23 15:36:39 +0300

    Update BusDriver.java

--- BusDriver.java
+++ BusDriver.java
@@ -2,7 +2,7 @@ package com.example.omnibus;
 
 import java.sql.Date;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
