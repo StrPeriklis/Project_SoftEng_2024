93177dbcaedf74e864063aaa98545a208b83fc12
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-23 20:24:34 +0300

    Update Bus.java

--- Bus.java
+++ Bus.java
@@ -4,3 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+@Entity
+public class Bus {
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
