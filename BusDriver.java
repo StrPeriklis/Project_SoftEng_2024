9a3b7aa829c2f08faf6b447c0f81cc226a1547ce
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-24 18:09:15 +0300

    Update BusDriver.java

---
+++ BusDriver.java
@@ -0,0 +1,12 @@
+package com.example.omnibus;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
