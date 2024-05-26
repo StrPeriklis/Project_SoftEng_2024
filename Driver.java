711a5548060f87c9b5950883a0360f76222378f5
Harrydimitro <133028382+Harrydimitro@users.noreply.github.com>
2024-05-26 20:00:11 +0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,15 @@
+package com.example.omnibus;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Driver {
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
