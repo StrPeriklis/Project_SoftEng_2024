cb04ab6bdec1abdc817071501f94b3da85f18d80
Harrydimitro <133028382+Harrydimitro@users.noreply.github.com>
2024-05-27 16:37:28 +0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -29,3 +29,28 @@ public class Driver {
 	}
 	public void setEmail(String email) {
 		this.email = email;
+
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getAfm() {
+		return afm;
+	}
+	public void setAfm(String afm) {
+		this.afm = afm;
+	}
+	
+	
+	
+	
+}
