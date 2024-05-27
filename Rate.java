dd6fa2fd5f0204bf25029046d36c269dd03a2595
PERIKLIS STRAVODIMOS <129185321+StrPeriklis@users.noreply.github.com>
2024-05-27 18:06:21 +0300

    Add files via upload

---
+++ Rate.java
@@ -0,0 +1,34 @@
+package com.example.omnibus;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Rate {
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	
+	private Integer gradebus;
+	private Integer gradeDriver;
+	private String Comment1;
+	private String Comment2;
+	private Date dt;
+	@ManyToOne
+	  @JoinColumn(name = "uid", referencedColumnName = "id")
+	private User usr;
+	
+	@ManyToOne
+	  @JoinColumn(name = "did", referencedColumnName = "id")
+	
+	private Driver drv;
+	@ManyToOne
+	  @JoinColumn(name = "bid", referencedColumnName = "id")
+	
+
