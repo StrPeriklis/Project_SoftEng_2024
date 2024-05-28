51a1a582a139e609405f05df1dadaa31b991a5b0
Harrydimitro <133028382+Harrydimitro@users.noreply.github.com>
2024-05-28 22:35:02 +0300

    Update Rate.java

--- Rate.java
+++ Rate.java
@@ -1,1 +1,16 @@
+package com.example.omnibus;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Rate {
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
