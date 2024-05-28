065be248d8fee67065f6876155d411016353dd5d
Harrydimitro <133028382+Harrydimitro@users.noreply.github.com>
2024-05-28 23:53:02 +0300

    Update Rate.java

--- Rate.java
+++ Rate.java
@@ -14,3 +14,22 @@ public class Rate {
 	@Id
 	  @GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
+private Integer gradebus;
+	private Integer gradeDriver;
+	private String Comment1;
+	private String Comment2;
+	private Date dt;
+	@ManyToOne
+	  @JoinColumn(name = "uid", referencedColumnName = "id")
+	private User usr;
+	
+	@ManyToOne
+	  @JoinColumn(name = "did", referencedColumnName = "id")
+	
+	private Driver drv;
+	@ManyToOne
+	  @JoinColumn(name = "bid", referencedColumnName = "id")
+	
+	private Bus b;
+	public Integer getId() {
+		return id;
