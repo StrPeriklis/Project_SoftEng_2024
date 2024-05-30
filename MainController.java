bef7e8ff64d0cf35d7d5860cd8881adf9e9b615a
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-05-31 02:38:49 +0300

    Update MainController.java

--- MainController.java
+++ MainController.java
@@ -10,3 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import org.springframework.stereotype.Controller;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+
