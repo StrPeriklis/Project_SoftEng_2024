8155a07b171a84d053116f1261428c15f931c060
Harrydimitro <133028382+Harrydimitro@users.noreply.github.com>
2024-05-31 16:17:24 +0300

    Update Station.java

--- Station.java
+++ Station.java
@@ -23,3 +23,21 @@ public class Station {
 	public String getTitle() {
 		return title;
 	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public float getLat() {
+		return lat;
+	}
+	public void setLat(float lat) {
+		this.lat = lat;
+	}
+	public float getLng() {
+		return lng;
+	}
+	public void setLng(float lng) {
+		this.lng = lng;
+	}
+	
+}
