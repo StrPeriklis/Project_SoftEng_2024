ae6a03961ac2b2810ba21c6c7d64d896d5bc4d0c
Konstantinos Filippopoulos <133027254+filippopouloskonstantinos@users.noreply.github.com>
2024-06-02 18:07:37 +0300

    Update Ticket.java

---
+++ Ticket.java
@@ -0,0 +1,32 @@
+package com.example.omnibus;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Ticket {
+
+	@Id
+	  @GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer id;
+	private Integer TicketNum;
+	private Date usedDate;
+	private Date ekdosi;
+	
+	@ManyToOne
+	  @JoinColumn(name = "lid", referencedColumnName = "id")
+	private Line L;
+	private String type;
+	@ManyToOne
+	  @JoinColumn(name = "uid", referencedColumnName = "id")
+	private User U;
+	
+	
+	
+}
